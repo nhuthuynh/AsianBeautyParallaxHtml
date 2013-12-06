23f1d70d70995e5c671400db174f20c08c88d4e4
Nhut Huynh <Ne7ven@yahoo.com>
2013-12-06 07:46:08 +0700

    finish index.html add parallax scroll, waypoint script to track

--- js/main.js
+++ js/main.js
@@ -1,1 +1,34 @@
+$(document).bind('ready', function () {
+    var $win = $(window),
+    section = $('#main .section'),
+    nav = $('#nav .navbar'),
+    links = $('#nav .navbar .nav-item'),
+    htmlbody = $('html, body');
 
+    $win.stellar({
+        horizontalScrolling: false,
+        verticalScrolling: true
+    });
+
+    section.waypoint(function (ev, dir) {
+        dataslide = $(this).attr('data-slide');
+
+        $('.nav-item[href*=' + dataslide + ']', nav).parent().addClass('active').siblings().removeClass('active');
+    });
+
+    $win.scroll(function () {
+        if ($win.scrollTop() == 0) {
+            $('.nav-item[href*="intro"]', nav).parent().addClass('active').siblings().removeClass('active');
+        }
+    });
+
+    function goToByScroll(dataslide) {
+        console.log(dataslide);
+        htmlbody.animate({
+            scrollTop: $('#main .section[data-slide="' + dataslide + '"]').offset().top
+        }, 2000, 'easeInOutQuint');
+    }
+
+    links.bind('click', function (e) { e.preventDefault(); goToByScroll($(this).attr('href')); return false; })
+
+});
