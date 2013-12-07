c7eac0bfb505bcaba8ed28e5417a9cc0fe11fad3
Nhut Huynh <Ne7ven@yahoo.com>
2013-12-07 07:07:07 +0700

    edit layout of section to fix the screen

--- js/main.js
+++ js/main.js
@@ -3,30 +3,42 @@ $(document).bind('ready', function () {
     section = $('#main .section'),
     nav = $('#nav .navbar'),
     links = $('#nav .navbar .nav-item'),
-    htmlbody = $('html, body');
+    htmlbody = $('html, body'),
+    lastScrollPos = 0,
+    listSections = ["intro", "exhibit", "visitor", "about"],
+    currentSection = 0, isScrollable = true;
 
     $win.stellar({
         horizontalScrolling: false,
         verticalScrolling: true
     });
 
-    section.waypoint(function (ev, dir) {
+    section.waypoint(function (ev, direction) {
+        console.log(direction, $('#header').outerHeight());
         dataslide = $(this).attr('data-slide');
-
         $('.nav-item[href*=' + dataslide + ']', nav).parent().addClass('active').siblings().removeClass('active');
     });
 
     $win.scroll(function () {
+        if (!isScrollable) return;
+        isScrollable = false;
         if ($win.scrollTop() == 0) {
             $('.nav-item[href*="intro"]', nav).parent().addClass('active').siblings().removeClass('active');
         }
+        console.log('before', currentSection);
+        if (lastScrollPos > $win.scrollTop()) currentSection--;
+        else currentSection++;
+        console.log('after', currentSection);
+        //goToByScroll(listSections[currentSection]);
     });
 
     function goToByScroll(dataslide) {
-        console.log(dataslide);
+        currentSection = listSections.indexOf(dataslide);
         htmlbody.animate({
             scrollTop: $('#main .section[data-slide="' + dataslide + '"]').offset().top
-        }, 2000, 'easeInOutQuint');
+        }, 2000, 'easeInOutQuint', function () {
+            isScrollable = true;
+        });
     }
 
     links.bind('click', function (e) { e.preventDefault(); goToByScroll($(this).attr('href')); return false; })
